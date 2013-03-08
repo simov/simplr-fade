131f4c066f9a492d87619f9ebc447cb9fbee3074
simov <simeonvelichkov@gmail.com>
2013-03-08 11:13:30 +0200

    Version 3.0 - support for css3 animations.

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ You can use whatever html structure you want to as long as there is a container 
 ```html
 <div class="fader">
     <div class="item">
-		<p>item 1</p>
+    	<p>item 1</p>
 	</div>
 	<!-- ... -->
 	<div class="item">
@@ -24,31 +24,43 @@ Use a list if you want to.
 </ul>
 ```
 
+
 ##Css
-The container must be **relative** and the items inside must be **absolute**.
+The container must be *relative* and the items inside must be *absolute*.
 ```css
 /*required*/
 .fader { position: relative; }
 .fader .item { position: absolute; }
 ```
-The items should have a **non transparent background** within them and their size should be equal to the size of the container, in order to *hide* them one below another.
+The items should have a *non transparent background* within them and their size should be equal to the size of the container, in order to *hide* them one below another.
+
 
 ##Plugin call
 ```js
 $('.fader .item').srFade();
 ```
 
+
 ##Properties
 
-####speed
-Alternate the animation speed (in milliseconds: 1000 = 1 second, 400 = 0.4 of a second).
-```js
-$('.fader .item').srFade({speed: 600});
-```
 ####duration
+Alternate the animation duration (in milliseconds: 1000 = 1 second, 400 = 0.4 of a second).
+```js
+$('.fader .item').srFade({duration: 600});
+```
+
+####delay
 Alternate the time to wait between each step when *auto* is on (in milliseconds: 1000 = 1 second, 400 = 0.4 of a second).
 ```js
-$('.fader .item').srFade({duration: 4000});
+$('.fader .item').srFade({delay: 4000});
+```
+
+####easing
+Specify an easing function to use for the transition between items. When using the *animate* property [jQuery Easing](http://gsgd.co.uk/sandbox/jquery/easing/) plugin must be included inside your page. When using the *css3* property no additional plugin is required.
+```js
+$('.fader .item').srFade({
+    easing: 'easeOutSine'
+});
 ```
 
 ####auto
@@ -65,30 +77,30 @@ $('.fader .item').srFade({
 });
 ```
 
-####easing
-Specify an easing function to use for the transition between items. Note that the **jquery.easing** plugin must be included in your page before that.
-```js
-$('.fader .item').srFade({
-	easing: 'easeOutBounce'
-});
-```
-
 ####effect
-Specify a predefined animation from the **jquery.ui.effect** plugin. Note that it must be included in your page before that.
+Specify a predefined animation from the [jQuery UI Effect](http://docs.jquery.com/UI/Effects) plugin. Note that it must be included inside your page before that.
 ```js
 $('.fader .item').srFade({
 	effect: 'puff'
 });
 ```
-Not all effects from the **jquery.ui.effect** plugin are useful for this plugin. The right ones to use are: *blind, clip, drop, explode, fade, fold, puff*.
+Not all effects from the *jQuery UI Effect* plugin are useful for this plugin. The right ones to use are: *blind, clip, drop, explode, fade, fold, puff*.
 
-The *animate* and the *effect* properties can't be used simultaneously. The *effect* property will take precedence if both are supplied.
+####css3
+Specify a predefined animation from [animate.css](http://daneden.me/animate/). Note that it must be included inside your page before that.
+```js
+$('.fader .item').srFade({
+    css3: 'flipOutX'
+});
+```
+
+The *animate*, *effect* and *css3* properties can't be used simultaneously. You can choose one of them for every plugin instance.
 
 
 ##Events
 
 ####onFade
-Raised just before the animation starts. It receives the item's **index** that is about to be animated. This is the place to update the navigation of the fader (if there is one).
+Raised just before the animation starts. It receives the item's *index* that is about to be animated. This is the place to update the navigation of the fader (if there is one).
 ```js
 $('.fader .item').srFade({
 	onFade: function (index) {
@@ -98,7 +110,7 @@ $('.fader .item').srFade({
 ```
 
 ####onAfterFade
-Raises after the animation of the current item is completed. It receives the current item's **index**. Can be used to start secondary animations within the fader items.
+Raises after the animation of the current item is completed. It receives the current item's *index*. Can be used to start secondary animations within the fader items.
 ```js
 $('.fader .item').srFade({
 	onAfterFade: function (index) {
@@ -114,6 +126,7 @@ var fader = $('.fader .item').srFade({
 	// ... properties and events
 });
 ```
+
 ####prev
 Navigation through the items back and forth. Usefull for navigation arrows.
 ```js
@@ -122,6 +135,7 @@ $('.prev').click(function () {
 	return false;
 });
 ```
+
 ####next
 ```js
 $('.next').click(function () {
@@ -129,6 +143,7 @@ $('.next').click(function () {
 	return false;
 });
 ```
+
 ####fadeTo
 Navigate to a specified item. Usefull for navigation dots.
 ```js
@@ -137,6 +152,7 @@ $('.fader-nav a').click(function () {
 	return false;
 });
 ```
+
 ####stop
 The automatic transition between items can be stopped temporarily and restarted back later on. Usefull when hovering on fader item.
 ```js
@@ -144,6 +160,7 @@ $('.fader .item').mouseover(function () {
 	fader.stop();
 });
 ```
+
 ####start
 ```js
 $('.fader .item').mouseout(function () {
--- examples/js/demo1.js
+++ examples/js/demo1.js
@@ -3,8 +3,15 @@
 
 $(function () {
     fader = $('.fader .item').srFade({
-        onFade : function (index) {
-            $('.controls .nav a').removeClass('active').eq(index).addClass('active');
+        css3: $('.css3 option:eq(0)').text(),
+        onFade: function (index) {
+            $('.nav a').removeClass('active').eq(index).addClass('active');
+        },
+        onAfterFade: function (index) {
+            if (this.cssChanged) {
+                cssInit();
+                this.cssChanged = false;
+            }
         }
     });
     $('.prev').on('click', function (e) {
@@ -34,154 +41,86 @@ $(function () {
 // demo
 
 $(function () {
-    // init
-    $('#controls input:eq(0)').attr('checked', true);
-    $('#controls label:eq(0)').addClass('active');
-    $('.effect').attr('disabled', true);
-    $('.animate').attr('disabled', true);
-    $('.effect')[0].selectedIndex = 0;
-    $('.animate')[0].selectedIndex = 3;
-    $('.speed')[0].selectedIndex = 4;
-    $('.duration')[0].selectedIndex = 0;
-    change.effect(0);
-    change.speed(4);
-    change.duration(0);
-    change.type(0);
-    change.easing(0);
-    
-    // events
-    $('[type=radio]').on('change', function () {
-        $('#controls label').removeClass('active');
-        $(this).next().addClass('active');
-        change.type($('[type=radio]').index(this));
-    });
-    $('.effect').on('change', function (e) {
-        change.effect($(this)[0].selectedIndex);
-    });
-    $('.animate').on('change', function (e) {
-        change.animate($(this)[0].selectedIndex);
-    });
-    $('.speed').on('change', function (e) {
-        change.speed($(this)[0].selectedIndex);
-    });
-    $('.duration').on('change', function (e) {
-        change.duration($(this)[0].selectedIndex);
-    });
-    $('.easing').on('change', function (e) {
-        change.easing($(this)[0].selectedIndex);
-    });
-    $('.easing-sub').on('change', function (e) {
-        change.subEasing();
-    });
-    $('.style-overflow input').on('click', function (e) {
-        $('.fader').toggleClass('overflow');
-    });
-    $('.style-images input').on('click', function (e) {
-        $('.fader .item img').toggleClass('wh100');
-    });
-    $('.create').on('click', function (e) {
-        change.create();
-    });
-});
 
-var change = {
-    type: function (index) {
+    // change transition
+
+    $('[name=transition]').on('change', function (e) {
+        // set active
+        $('fieldset').removeClass('active');
+        $(this).attr('checked', true).parents('fieldset').addClass('active');
+        // animation helpers
         $('.fader').removeClass('overflow');
         $('.fader .item img').removeClass('wh100');
         $('.custom').slideUp();
-        switch(index) {
+        // change
+        switch($('[name=transition]').index(this)) {
             case 0: //default
                 fader.effect = null;
+                fader.css3 = null;
                 fader.animate = {opacity: 'hide'};
-                $('.effect, .animate').attr('disabled', true);
-                $('.easing').attr('disabled', false);
-                this.easing($('.easing')[0].selectedIndex);
+                // dropdowns
+                $('.animate, .effect, .css3').attr('disabled', true);
+                $('.easing, .easing option').attr('disabled', false);
+                // easing
+                $('.easing').trigger('change');
                 break;
-            case 1: //effect
-                this.effect($('.effect')[0].selectedIndex);
+            case 1: //animate
+                fader.effect = null;
+                fader.css3 = null;
+                $('.animate').trigger('change');
+                // dropdowns
+                $('.animate').attr('disabled', false);
+                $('.effect, .css3').attr('disabled', true);
+                // easing
+                $('.easing, .easing option').attr('disabled', false);
+                $('.easing').trigger('change');
+                break;
+            case 2: //effect
+                fader.css3 = null;
+                $('.effect').trigger('change');
+                // dropdowns
                 $('.effect').attr('disabled', false);
-                $('.animate').attr('disabled', true);
+                $('.animate, .css3').attr('disabled', true);
+                // easing
                 $('.easing, .easing-sub').attr('disabled', true);
                 break;
-            case 2: //animate
+            case 3: //css3
                 fader.effect = null;
-                this.animate($('.animate')[0].selectedIndex);
-                $('.animate').attr('disabled', false);
-                $('.effect').attr('disabled', true);
-                $('.easing').attr('disabled', false);
-                this.easing($('.easing')[0].selectedIndex);
+                $('.css3').trigger('change');
+                // dropdowns
+                $('.css3').attr('disabled', false);
+                $('.animate, .effect').attr('disabled', true);
+                // easing
+                $('.easing, .easing-sub').attr('disabled', false);
+                $('.easing option').first().attr('disabled', true);
+                $('.easing option').last().attr('disabled', true);
+                $('.easing option').last().prev().attr('disabled', true);
+                if ($('.easing option:selected').attr('disabled') == 'disabled') {
+                    $('.easing option:eq(1)').attr('selected', true);
+                }
+                $('.easing').trigger('change');
                 break;
+            
         }
-    },
-    effect: function (index) {
-        switch(index) {
-            case 0:  fader.effect = 'puff';
-                $('.fader .item img').addClass('wh100'); break;
-            case 1:  fader.effect = 'blind';   break;
-            case 2:  fader.effect = 'clip';    break;
-            case 3:  fader.effect = 'drop';    break;
-            case 4:  fader.effect = 'explode'; break;
-            case 5:  fader.effect = 'fade';    break;
-            case 6:  fader.effect = 'fold';    break;
-            default: fader.effect = 'puff';    break;
-        }
-    },
-    animation: [
-        {def: {width:0 }},
-        {def: {height:0 }},
-        {def: {width:0,height:0 }},
-        {def: {width:0,height:0,top:313/2,left:500/2,opacity:0.5 }},
-        {def: {top:-313,opacity:0 }},
-        {def: {top:-313 }, fader:true},
-        {def: {left:-500 }, fader:true},
-        {def: {width:832,height:521,top:-104,left:-166,opacity:0 }, images:true}
-    ],
-    animate: function (index) {
-        $('.fader').removeClass('overflow');
-        $('.fader .item img').removeClass('wh100');
-        
-        var overflow = this.animation[index].fader || false,
-            images = this.animation[index].images || false;
+    });
 
-        overflow ? $('.fader').addClass('overflow') : null;
-        $('.style-overflow input').attr('checked', overflow);
-        images ? $('.fader .item img').addClass('wh100') : null;
-        $('.style-images input').attr('checked', images);
+    // options
 
-        $('.custom').slideDown();
-        $('#controls textarea').val(JSON.stringify(this.animation[index].def));
-        fader.animate = this.animation[index].def;
-    },
-    create: function () {
-        $('#controls p').hide();
-        try {
-            var animation = JSON.parse($('#controls textarea').val());
-            fader.animate = animation;
-        } catch (err) {
-            $('#controls p').show();
+    $('.duration').on('change', function (e) {
+        var value = $('.duration option:selected').text();
+        fader.duration = parseInt(value);
+        if (fader.css3) {
+            $('.fader .item').css(prefix+'animation-duration', value+'ms');
         }
-    },
-    speed: function (index) {
-        switch(index) {
-            case 0:  fader.speed = 200;  break;
-            case 1:  fader.speed = 400;  break;
-            case 2:  fader.speed = 800;  break;
-            case 3:  fader.speed = 1000; break;
-            case 4:  fader.speed = 1200; break;
-            default: fader.speed = 400;  break;
-        }
-    },
-    duration: function (index) {
-        switch(index) {
-            case 0:  fader.duration = 1000; break;
-            case 1:  fader.duration = 2000; break;
-            case 2:  fader.duration = 3000; break;
-            case 3:  fader.duration = 4000; break;
-            case 4:  fader.duration = 5000; break;
-            default: fader.duration = 3000; break;
-        }
-    },
-    easing: function (index) {
+    });
+
+    $('.delay').on('change', function (e) {
+        var value = $('.delay option:selected').text();
+        fader.delay = parseInt(value);
+    });
+
+    $('.easing').on('change', function (e) {
+        var index = $('.easing option').index($('.easing option:selected'));
         if (index == 0 || index == 1) {
             $('.easing-sub').attr('disabled', true);
             fader.easing = $('.easing option:selected').val();
@@ -189,10 +128,147 @@ var change = {
             $('.easing-sub').attr('disabled', false);
             fader.easing = $('.easing-sub option:selected').val()
                             + $('.easing option:selected').val();
+            if (fader.css3) {
+                $('.fader .item').css(prefix+'animation-timing-function', cssEasing());
+            }
         }
-    },
-    subEasing: function (index) {
+    });
+
+    $('.easing-sub').on('change', function (e) {
         fader.easing = $('.easing-sub option:selected').val()
                         + $('.easing option:selected').val();
+        if (fader.css3) {
+            $('.fader .item').css(prefix+'animation-timing-function', cssEasing());
+        }
+    });
+
+    // transitions
+
+    var animation = [
+        {def: {width:0 }},
+        {def: {height:0 }},
+        {def: {width:0,height:0 }},
+        {def: {width:0,height:0,top:313/2,left:500/2,opacity:0.5 }},
+        {def: {top:-313,opacity:0 }},
+        {def: {top:-313 }, overflow:true},
+        {def: {left:-500 }, overflow:true},
+        {def: {width:832,height:521,top:-104,left:-166,opacity:0 }, images:true}
+    ];
+
+    $('.animate').on('change', function (e) {
+        var index = $('.animate option').index($('.animate option:selected'));
+        $('.fader').removeClass('overflow');
+        $('.fader .item img').removeClass('wh100');
+        
+        var anim = animation[index],
+            overflow = anim.overflow || false,
+            images = anim.images || false;
+
+        overflow ? $('.fader').addClass('overflow') : null;
+        $('.style-overflow input').attr('checked', overflow);
+        images ? $('.fader .item img').addClass('wh100') : null;
+        $('.style-images input').attr('checked', images);
+
+        $('.custom').slideDown();
+        $('#controls textarea').val(JSON.stringify(anim.def, null, 4));
+        fader.animate = anim.def;
+    });
+
+    $('.effect').on('change', function (e) {
+        var name = $('.effect option:selected').text();
+        name == 'puff'
+            ? $('.fader .item img').addClass('wh100')
+            : $('.fader .item img').removeClass('wh100');
+        fader.effect = name;
+    });
+
+    $('.css3').on('change', function (e) {
+        // inline
+        fader.cssChanged = true;
+        // set transition
+        var name = $('.css3 option:selected').text();
+        fader.css3 = name;
+    });
+
+    // animation helpers
+
+    $('.style-overflow input').on('click', function (e) {
+        $('.fader').toggleClass('overflow');
+    });
+
+    $('.style-images input').on('click', function (e) {
+        $('.fader .item img').toggleClass('wh100');
+    });
+
+    $('.create').on('click', function (e) {
+        $('#controls p').hide();
+        try {
+            var animation = JSON.parse($('#controls textarea').val());
+            fader.animate = animation;
+        } catch (err) {
+            $('#controls p').show();
+        }
+    });
+
+    // init
+
+    $('.duration option:eq(4)').attr('selected', true).parent().trigger('change');
+    $('.delay option:eq(0)').attr('selected', true).parent().trigger('change');
+    $('[name=transition]:eq(0)').trigger('change');
+});
+
+// css3 helpers
+
+var prefix = (function cssPrefix () {
+    switch (true) {
+        case $.browser.webkit: return '-webkit-';
+        case $.browser.mozilla: return '-moz-';
+        case $.browser.msie: return '-ms-';
+        case $.browser.opera: return '-o-';
+        default: return '';
     }
-};
+}());
+
+function cssInit () {
+    var items = $('.fader .item');
+    for (var i=0; i < items.length; i++) {
+        var display = items[i].style.display,
+            zIndex = items[i].style.zIndex;
+        items[i].style.cssText = '';
+        $(items[i]).css({zIndex: zIndex, display: display})
+            .css(prefix+'backface-visibility', 'hidden')
+            .css(prefix+'animation-duration', fader.duration+'ms')
+            .css(prefix+'animation-timing-function', cssEasing())
+            .css(prefix+'animation-fill-mode', 'both');
+    }
+}
+
+function cssEasing () {
+    switch (fader.easing) {
+        case 'easeInSine': return 'cubic-bezier(0.47, 0, 0.745, 0.715)';
+        case 'easeOutSine': return 'cubic-bezier(0.39, 0.575, 0.565, 1)';
+        case 'easeInOutSine': return 'cubic-bezier(0.445, 0.05, 0.55, 0.95)';
+        case 'easeInQuad': return 'cubic-bezier(0.55, 0.085, 0.68, 0.53)';
+        case 'easeOutQuad': return 'cubic-bezier(0.25, 0.46, 0.45, 0.94)';
+        case 'easeInOutQuad': return 'cubic-bezier(0.455, 0.03, 0.515, 0.955)';
+        case 'easeInCubic': return 'cubic-bezier(0.55, 0.055, 0.675, 0.19)';
+        case 'easeOutCubic': return 'cubic-bezier(0.215, 0.61, 0.355, 1)';
+        case 'easeInOutCubic': return 'cubic-bezier(0.645, 0.045, 0.355, 1)';
+        case 'easeInQuart': return 'cubic-bezier(0.895, 0.03, 0.685, 0.22)';
+        case 'easeOutQuart': return 'cubic-bezier(0.165, 0.84, 0.44, 1)';
+        case 'easeInOutQuart': return 'cubic-bezier(0.77, 0, 0.175, 1)';
+        case 'easeInQuint': return 'cubic-bezier(0.755, 0.05, 0.855, 0.06)';
+        case 'easeOutQuint': return 'cubic-bezier(0.23, 1, 0.32, 1)';
+        case 'easeInOutQuint': return 'cubic-bezier(0.86, 0, 0.07, 1)';
+        case 'easeInExpo': return 'cubic-bezier(0.95, 0.05, 0.795, 0.035)';
+        case 'easeOutExpo': return 'cubic-bezier(0.19, 1, 0.22, 1)';
+        case 'easeInOutExpo': return 'cubic-bezier(1, 0, 0, 1)';
+        case 'easeInCirc': return 'cubic-bezier(0.6, 0.04, 0.98, 0.335)';
+        case 'easeOutCirc': return 'cubic-bezier(0.075, 0.82, 0.165, 1)';
+        case 'easeInOutCirc': return 'cubic-bezier(0.785, 0.135, 0.15, 0.86)';
+        case 'easeInBack': return 'cubic-bezier(0.6, -0.28, 0.735, 0.045)';
+        case 'easeOutBack': return 'cubic-bezier(0.175, 0.885, 0.32, 1.275)';
+        case 'easeInOutBack': return 'cubic-bezier(0.68, -0.55, 0.265, 1.55)';
+        default: return 'linear';
+    }
+}
